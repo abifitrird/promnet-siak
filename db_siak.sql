f19521a36881129925c0e4b28241b627c1467176
abifitrird <fratnadewi@gmail.com>
2018-11-12 17:27:37 +0700

    Add files via upload
    database

--- db_siak.sql
+++ db_siak.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 11, 2018 at 03:23 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Nov 12, 2018 at 11:27 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,18 +27,80 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admins` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(225) NOT NULL,
-  `username` varchar(225) NOT NULL,
-  `password` varchar(225) NOT NULL
+  `id_username` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `nama_lengkap` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admins`
 --
 
-INSERT INTO `admins` (`id`, `nama`, `username`, `password`) VALUES
-(1, 'Fitri RD', 'fitrird', '16d4f73f24060569c9b5406b33ada24b');
+INSERT INTO `admins` (`id_username`, `username`, `password`, `nama_lengkap`) VALUES
+(1, 'hilda', '202cb962ac59075b964b07152d234b70', 'hilda aulia safitri'),
+(2, 'hildaaul', '25d55ad283aa400af464c76d713c07ad', 'hilda aulia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_dosen`
+--
+
+CREATE TABLE `tb_dosen` (
+  `id_dosen` int(11) NOT NULL,
+  `kode_dosen` varchar(100) DEFAULT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `matkul` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tb_dosen`
+--
+
+INSERT INTO `tb_dosen` (`id_dosen`, `kode_dosen`, `nama`, `matkul`) VALUES
+(1, '001', 'abcd', 'pemrograman internet'),
+(2, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_mahasiswa`
+--
+
+CREATE TABLE `tb_mahasiswa` (
+  `id_mahasiswa` int(11) NOT NULL,
+  `nim` varchar(50) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `kelas` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_matkul`
+--
+
+CREATE TABLE `tb_matkul` (
+  `id_matkul` int(11) NOT NULL,
+  `kode_matkul` varchar(50) DEFAULT NULL,
+  `nama_matkul` varchar(50) DEFAULT NULL,
+  `jumlah_sks` varchar(50) DEFAULT NULL,
+  `kelas` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_prodi`
+--
+
+CREATE TABLE `tb_prodi` (
+  `id_prodi` int(11) NOT NULL,
+  `kode_prodi` varchar(50) DEFAULT NULL,
+  `nama_prodi` varchar(50) DEFAULT NULL,
+  `fakultas` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -48,7 +110,31 @@ INSERT INTO `admins` (`id`, `nama`, `username`, `password`) VALUES
 -- Indexes for table `admins`
 --
 ALTER TABLE `admins`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_username`);
+
+--
+-- Indexes for table `tb_dosen`
+--
+ALTER TABLE `tb_dosen`
+  ADD PRIMARY KEY (`id_dosen`);
+
+--
+-- Indexes for table `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  ADD PRIMARY KEY (`id_mahasiswa`);
+
+--
+-- Indexes for table `tb_matkul`
+--
+ALTER TABLE `tb_matkul`
+  ADD PRIMARY KEY (`id_matkul`);
+
+--
+-- Indexes for table `tb_prodi`
+--
+ALTER TABLE `tb_prodi`
+  ADD PRIMARY KEY (`id_prodi`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -58,7 +144,27 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_username` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tb_dosen`
+--
+ALTER TABLE `tb_dosen`
+  MODIFY `id_dosen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tb_mahasiswa`
+--
+ALTER TABLE `tb_mahasiswa`
+  MODIFY `id_mahasiswa` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tb_matkul`
+--
+ALTER TABLE `tb_matkul`
+  MODIFY `id_matkul` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tb_prodi`
+--
+ALTER TABLE `tb_prodi`
+  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
